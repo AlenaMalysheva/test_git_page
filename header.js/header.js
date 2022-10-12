c9c0c7c9fc3187e8096f17276de901f7127c63f5
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2022-10-12 16:46:23 +0300

    style css

--- header.js/header.js
+++ header.js/header.js
@@ -1,5 +1,10 @@
 const root = document.querySelector('#root');
+const list_el = document.querySelector('li');
 
+
+list_el.addEventListener('click', () => {
+    list_el.classList.toggle('item_menu');
+})
 
 
 
